60a2b4bb1a3e9b13ae75297a65cc0766ae0f919f
wasdfg <leejh9711@naver.com>
2024-09-07 23:24:46 +0900

    json형식의 파일을 읽어들이는 코드

--- python/saveDatabyJson.py
+++ python/saveDatabyJson.py
@@ -25,3 +25,27 @@ def writeData():
     fs.release() #파일저장
     
 writeData()
+
+def readData():
+    fs = cv.FileStorage(filename, cv.FileStorage_READ) #파일을 읽어들임
+    
+    if not fs.isOpened():
+        print("file open failed")
+        return
+    
+    name = fs.getNode('name').string() #읽은 파일에 있는 변수를 읽어온다
+    age = int(fs.getNode('age').real())
+    pt1 = tuple(fs.getNode('point').mat().astype(np.int32).flatten().tolist())
+    scores = tuple(fs.getNode('scores').mat().flatten().tolist())
+    mat1 = fs.getNode('data').mat()
+    
+    fs.release()
+    
+    print('name:',name)
+    print('age:',age)
+    print('point:',pt1)
+    print('scores:',scores)
+    print('data:')
+    print(mat1)
+
+readData()
