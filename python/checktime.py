62905c87d9f19c771aea1f6312a2b2c289c7dc63
wasdfg <leejh9711@naver.com>
2024-09-08 22:05:55 +0900

    함수 동작 시간 계산

--- python/checktime.py
+++ python/checktime.py
@@ -7,17 +7,17 @@ if img is None:
     print("img load failed")
     exit()
  
-dst = np.empty(img.shape,dtype=img.dtype)
+dst = np.empty(img.shape,dtype=img.dtype) #이미지 파일의 크기만큼의 빈 numpy를 생성
 dst1 = np.empty(img.shape,dtype=img.dtype)
 
 tm = cv.TickMeter()
 tm.start()
-dst = ~img
+dst = ~img #색반전
 tm.stop()
 print(tm.getTimeMilli()/1000)
 
 tm.start()
-for y in range(img.shape[0]):
+for y in range(img.shape[0]): #색반전을 np를 사용하지 않고 for문으로 
     for x in range(img.shape[1]):
         dst1[y,x] = 255 - img[y,x]
 tm.stop()
